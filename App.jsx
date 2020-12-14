342614d130d70b780c28caeec5c88b056dc99fdc
danilads <vfhn3fghtkm>
2020-12-14 18:47:36 +0300

    upd

---
+++ App.jsx
@@ -0,0 +1,13 @@
+﻿//react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/Main';
+
+//css
+import './scss/main.scss';
+ReactDOM.render(
+	<Main/>
+	, document.getElementById('container') 
+);
+
---
+++ components/Main.jsx
@@ -0,0 +1,22 @@
+import React,{Fragment} from 'react';
+import Filter from '../primitives/Filter';
+import axios from "axios";
+
+class Main extends React.PureComponent {
+  	render() {
+
+		return (
+			<div className={"Block_Filter"}>
+				<Fragment>
+					<Filter>hello</Filter>
+				</Fragment>
+			</div>
+		);
+
+  	}
+
+}
+
+
+
+export default Block_MainPage;
